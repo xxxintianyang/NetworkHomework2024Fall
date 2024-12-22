3f731ecd9e32be62a7bbd07f5f9f921f240f5494
遠野 <heheeryila@gmail.com>
2024-12-23 03:05:30 +0800

    网络访问数据库测试v1.0

--- web-db-control/server/config/db.config.js
+++ web-db-control/server/config/db.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  host: "localhost",
-  user: "root",
-  password: "mayinan2004",  // 修改为您的MySQL密码
+  host: "0.0.0.0",
+  user: "newuser",
+  password: "password",  // 修改为您的MySQL密码
   database: "web_db_control",
   port: 3306,
   timezone: '+08:00'
